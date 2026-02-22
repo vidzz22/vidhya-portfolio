bfa87136d19d0e84025b8f6fb83a9d44b1e70957
VidhyaWalke <mscds.2420@unigoa.ac.in>
2026-02-22 21:51:11 +0530

    initial portfolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Vidhya Walke
+      </h1>
+    </div>
+  )
+}
+
+export default App
